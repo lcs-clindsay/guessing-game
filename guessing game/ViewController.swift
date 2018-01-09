d5fea74f04610d496c569ca8b3de9213dd23ce32
Lindsay, Calum <clindsay@lcs-mba3123.lcs.on.ca>
2018-01-09 10:50:00 -0500

    Initial Commit

---
+++ guessing game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  guessing game
+//
+//  Created by Lindsay, Calum on 2018-01-09.
+//  Copyright © 2018 Lindsay, Calum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
